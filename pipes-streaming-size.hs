f07e7166daaec5fdea21ec4cfabebd3de90c2ac5
Eugene <e@egeek.me>
2022-05-15 14:31:36 +0300

    [DOES NOT WORK] Attempt to use `StateT` directly

--- pipes-streaming-size.hs
+++ pipes-streaming-size.hs
@@ -1,9 +1,25 @@
 #!/usr/bin/env stack
--- stack --resolver lts-19.6 script --package pipes,pipes-bytestring,pipes-http
+-- stack --resolver lts-19.6 script --package bytestring,mtl,pipes,pipes-bytestring,pipes-http
 
+import Control.Monad.State.Strict
 import Pipes
 import Pipes.HTTP
+import qualified Data.ByteString as BL
 import qualified Pipes.ByteString as PB
+
+type Size = Int
+
+-- | Prints the total size of the downloaded stream so far when each new piece
+-- is received.
+printTotalSize :: Pipe BL.ByteString BL.ByteString (StateT Size IO) ()
+printTotalSize = forever $ do
+  bs <- await
+
+  modify' (+ (BL.length bs))
+  size <- get
+  liftIO $ print size
+
+  yield bs
 
 saveStream :: String -> IO ()
 saveStream url = do
@@ -11,8 +27,9 @@ saveStream url = do
   req <- parseUrlThrow url
 
   withHTTP req manager $ \resp ->
-    runEffect
+    flip evalStateT 0 $ runEffect
       $ responseBody resp
+      >-> printTotalSize
       >-> PB.stdout
 
 main :: IO ()
